d3c015083f559ce6ae979398e724da12369c6551
gabeboileau <gabeboileau@gmail.com>
2020-02-22 17:27:20 -0500

    Adds the first api chunk for the project.

---
+++ water-mango-api/water-mango-api/Controllers/PlantController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using water_mango_api.Models;
+
+namespace water_mango_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public Plant Get()
+        {
+            return new Plant("Something");
+        }
+
+    }
+}
---
+++ water-mango-api/water-mango.contracts/Models/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace water_mango_api.Models
+{
+    public class Plant
+    {
+        public Plant(String name)
+        {
+            this.name = name;
+
+            // setting the default last watered to now.
+            this.lastWatered = DateTime.Now;
+        }
+
+        public String name { get; set; }
+
+        public DateTime lastWatered { get; set; }
+
+    }
+}
